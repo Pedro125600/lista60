65e7c15f73efc3b53ba7bda57566f68899634e51
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-04-16 18:06:10 -0300

    Add files via upload

---
+++ att22.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double nun = 0;
+            double soma = 0;
+            int cont = -1;
+            do
+            {
+                soma += nun;
+                Console.WriteLine("De notas de 10 a 20 qualquer outro valor para parar");
+                nun = double.Parse(Console.ReadLine());
+          
+                cont++;
+
+            } while (nun >= 10 && nun <= 20);
+
+
+            double media = soma / cont;
+            Console.WriteLine($"A media das notas e de : {media} ");
+
+            Console.ReadLine();
+         
+
+            }
+          
+        }
+   }
+
