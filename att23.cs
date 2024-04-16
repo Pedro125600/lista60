56e1a67173453f1f776ca13904e6b69c09e69fff
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-04-16 18:13:17 -0300

    Add files via upload

---
+++ att23.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite um numero Positivo");
+            int nun = int.Parse(Console.ReadLine());
+
+            int cont = 1; 
+            while(cont <= nun)
+            {
+                if(nun % cont == 0)
+                {
+                    Console.WriteLine(cont);
+                }
+
+                cont++;
+
+            }
+
+
+
+
+
+
+            Console.ReadLine();
+         
+
+            }
+          
+        }
+   }
+
