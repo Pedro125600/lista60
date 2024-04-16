bdb7426d5f64130f8236498bf61cbd82c5dbd255
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-04-16 17:28:13 -0300

    Add files via upload

---
+++ att19.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Imprima um numero entre 100 e 999");
+            int n = int.Parse(Console.ReadLine());
+            while(n < 100 || n > 999)
+            {
+                Console.WriteLine("Valor invalido");
+                Console.WriteLine("Imprima um numero entre 100 e 999");
+                 n = int.Parse(Console.ReadLine());
+            }
+
+            int n1 = n / 100;
+            Console.WriteLine(n1);
+            int n2 = (n / 10) % 10;
+            Console.WriteLine(n2);
+            int n3 = n % 10;
+            Console.WriteLine(n3);
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
