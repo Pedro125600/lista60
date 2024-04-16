a6daa0d58217cf32dd7a96097098540e087b8046
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-04-16 18:23:17 -0300

    Add files via upload

---
+++ att25.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            int nun = 1000;
+            int cont = 1;
+            int soma = 0;
+            while(cont < nun)
+            {
+                if(cont % 3 == 0 || cont % 5 == 0)
+                {
+
+                    soma += cont;
+                }
+
+                cont++;
+
+            }
+
+            Console.WriteLine($"A soma de todos os numeros natrais abaixo de 1000 multiplos de 3 ou 5 e : {soma}");
+
+
+
+
+            Console.ReadLine();
+         
+
+            }
+          
+        }
+   }
+
