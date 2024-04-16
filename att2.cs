7f9ee03fc9229294982670f33dba052a9f72d1c1
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-04-16 17:09:27 -0300

    Update att2.cs

--- att2.cs
+++ att2.cs
@@ -11,9 +11,40 @@ namespace att2
         static void Main(string[] args)
         {
 
+ int i, j = 1;
+            int k = 1;
 
+            Console.WriteLine("for");
+            for (i = 1; i <= 100; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+ 
+
+            Console.WriteLine("\n While");
+            while (j <= 100)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+
+
+
+            Console.WriteLine("\n do While");
+            do
+            {
+                Console.WriteLine(k);
+                k++;
+
+
+
+            } while (k <= 100);
 
             Console.ReadLine();
+
+
+           
         }
     }
 }
