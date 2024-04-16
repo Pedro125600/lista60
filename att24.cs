d9d732a9878fb9aee83e895f2579dbb9b0920686
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-04-16 18:15:25 -0300

    Add files via upload

---
+++ att24.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite um numero Positivo");
+            int nun = int.Parse(Console.ReadLine());
+
+            int cont = 1;
+            int soma = 0;
+            while(cont < nun)
+            {
+                if(nun % cont == 0)
+                {
+
+                    soma += cont;
+                }
+
+                cont++;
+
+            }
+
+            Console.WriteLine($"A soma dos divisores de {nun} e: {soma}");
+
+
+
+
+            Console.ReadLine();
+         
+
+            }
+          
+        }
+   }
+
