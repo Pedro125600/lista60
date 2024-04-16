d8efd70c0ee30466992dc0b6cdb4f8e818ddfc2b
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-04-16 18:39:27 -0300

    Add files via upload

---
+++ att27.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Digite um numero Positivo inteiro");
+            int nun = int.Parse(Console.ReadLine());
+            int cont = 1 ;
+            int soma = 0;
+
+            while(nun != 0)
+            {
+                soma += 1/cont + 1;
+                cont++;
+                nun--;
+            }
+
+
+            Console.WriteLine($" a soma da serie harmonica e : {soma}");
+
+
+            Console.ReadLine();
+         
+
+            }
+          
+        }
+   }
+
