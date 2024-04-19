f57da4e357d26d7fb69cd95ef9379464f464c7f0
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-04-19 20:17:03 -0300

    Add files via upload

---
+++ att32.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace att32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            int d1 = r.Next(1,6);
+            int d2 = r.Next(1,6);
+            Console.WriteLine($"d1: {d1}");
+            Console.WriteLine($"d2: {d2}");
+
+            if (d1 > d2)
+            {
+                Console.WriteLine("d1 > d2");
+            }
+           
+           else if  (d2 > d1)
+            {
+                Console.WriteLine("d2 > d1");
+            }
+
+            else
+            {
+                Console.WriteLine("d1 = d2");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
