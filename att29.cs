6281816efb9bffbdac05370c8d047a10cfb77b23
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-04-19 18:43:39 -0300

    Add files via upload

---
+++ att29.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            double S = 0;
+            int valorf1 = 2;
+
+            for (i = 1; i <= 5; i++)
+            {
+                int fatorial = 1;
+
+                for (int j = valorf1; j > 0; j--)
+                {
+                    fatorial *= j;
+                }
+
+                S += i / (double)fatorial;
+                valorf1 += 2;
+            }
+
+            Console.WriteLine($"O valor de S é {S}");
+            Console.ReadLine();
+
+        }
+    }
+}
