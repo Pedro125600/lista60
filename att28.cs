41ff3691d129bfd6aeeec0c5064c168596816c02
Pedro Henrique Rodrigues Evangelista <99209592+Pedro125600@users.noreply.github.com>
2024-04-19 18:45:41 -0300

    Add files via upload

---
+++ att28.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista60
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i;
+            Console.WriteLine("um valor n inteiro e positivo");
+            int n = int.Parse(Console.ReadLine());
+
+            while (n < 0)
+            {
+                Console.WriteLine("um valor n inteiro e positivo");
+                n = int.Parse(Console.ReadLine());
+            }
+
+            double E = 1;
+            int fatorial = 1;
+            int valorf1 = n;
+
+
+            for (i = n; i > 0; i--)
+            {
+                valorf1 = i;
+                fatorial = 1;
+
+                while (valorf1 != 1)
+                {
+                    fatorial *= valorf1;
+                    valorf1--;
+
+                }
+
+
+
+                E += 1 / (double)fatorial;
+
+
+            }
+
+            Console.WriteLine($"\nO valor de S e {E} ");
+            Console.ReadLine();
+
+      
+
+        }
+    }
+}
